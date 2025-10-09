8814fd43503b5ab724e29a77cb15b5350fc3394e
Daniel B <danbeer31@gmail.com>
2025-10-08 22:39:14 -0500

    Updated.  Added admin/catalog

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,18 @@ use App\Http\Controllers\Admin\SupplierCatalogController;
 
 Route::get('/', [ShopController::class, 'home'])->name('home');
 Route::get('/product/{product:slug}', [ShopController::class, 'show'])->name('product.show');
-// --- new: AJAX color data endpoint ---
 Route::post('/product/color-data', [ShopController::class, 'colorData'])->name('product.colorData');
 
-
-// Admin (we’ll protect with auth later)
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard');
-    Route::get('/catalog/sanmar',        [SupplierCatalogController::class, 'sanmarIndex'])->name('sanmar.index');
-    Route::post('/catalog/sanmar/search',[SupplierCatalogController::class, 'sanmarSearch'])->name('sanmar.search');
-    Route::post('/catalog/sanmar/import',[SupplierCatalogController::class, 'sanmarImport'])->name('sanmar.import');
 
-    Route::get('/catalog/ss',            [SupplierCatalogController::class, 'ssIndex'])->name('ss.index');
-    Route::post('/catalog/ss/import',    [SupplierCatalogController::class, 'ssImport'])->name('ss.import');
+    // NEW: catalog index (lists imported products + links to imports)
+    Route::get('/catalog', [SupplierCatalogController::class, 'index'])->name('catalog.index');
+
+    Route::get('/catalog/sanmar',         [SupplierCatalogController::class, 'sanmarIndex'])->name('sanmar.index');
+    Route::post('/catalog/sanmar/search', [SupplierCatalogController::class, 'sanmarSearch'])->name('sanmar.search');
+    Route::post('/catalog/sanmar/import', [SupplierCatalogController::class, 'sanmarImport'])->name('sanmar.import');
+
+    Route::get('/catalog/ss',             [SupplierCatalogController::class, 'ssIndex'])->name('ss.index');
+    Route::post('/catalog/ss/import',     [SupplierCatalogController::class, 'ssImport'])->name('ss.import');
 });
